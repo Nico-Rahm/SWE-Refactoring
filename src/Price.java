ca3d4798070921f035c09182ace1a32c70eac92f
Nico-Rahm <65405346+Nico-Rahm@users.noreply.github.com>
2021-05-05 09:59:46 +0200

    Add files via upload

---
+++ src/Price.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Berkling
+ * Date: 14.05.14
+ * Time: 12:48
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Price {
+    int getPriceCode();
+    double getCharge(int daysRented);
+    int getFrequentRenterPoints(int daysRented);
+}
