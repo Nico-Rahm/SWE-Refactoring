7ccb138c5bda3b92f8d6aff00e10d3af7da988ce
Nico Rahm <nico_rahm@hotmail.de>
2021-05-11 22:56:10 +0200

    removed unused variable "totalAmount"

--- src/Customer.java
+++ src/Customer.java
@@ -18,7 +18,6 @@ class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
         Enumeration enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
